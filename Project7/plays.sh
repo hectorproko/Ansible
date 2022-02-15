43bbfd310abf2a21701893f3d87023f5b0779809
Hector Rodriguez <hectiquin2003@hotmail.com>
2022-02-14 19:48:48 -0500

    web page up SElinux disable

--- Project7/plays.sh
+++ Project7/plays.sh
@@ -1,13 +1,9 @@
+
+
 output=$(sudo ansible-playbook provisioning.yml)
 IP=$(echo $output | grep private_ip | cut -d '"' -f 4) #gets IP from output
 sudo ansible-playbook NFSconfigure.yml
 sudo ansible-playbook webConfigure.yml --extra-vars "privateIP=$IP" 
 
-IP=$(echo $output | grep private_ip | cut -d '"' -f 4)
-
-#ansible-playbook partest.yml --extra-vars "privateIP='$IP'” #works
-#ansible-playbook partest.yml --extra-vars "privateIP=$IP” #works
-#ansible-playbook partest.yml --extra-vars 'privateIP=$IP' #doesnt work
-#ansible-playbook partest.yml --extra-vars 'privateIP="$IP"' #doesnt work
-
-#cat output.txt | grep private_ip | cut -d '"' -f 4
+#IP=$(echo $output | grep private_ip | cut -d '"' -f 4)
+#sudo ansible-playbook webConfigure.yml --extra-vars "privateIP=172.31.94.32" 
