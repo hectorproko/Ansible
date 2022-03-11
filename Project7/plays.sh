e5f2a64dafeee64bfdedfbfb1ebf658695be3b90
Hector Rodriguez <hectiquin2003@hotmail.com>
2022-03-11 16:14:59 -0500

    Moved keys and dynamic inventory outside repo

--- Project7/plays.sh
+++ Project7/plays.sh
@@ -1,6 +1,7 @@
 
 
 output=$(sudo ansible-playbook provisioning.yml)
+echo "$output"
 IP=$(echo $output | grep private_ip | cut -d '"' -f 4) #gets IP from output
 sudo ansible-playbook NFSconfigure.yml
 sudo ansible-playbook webConfigure.yml --extra-vars "privateIP=$IP" 
--- Project8/plays.sh
+++ Project8/plays.sh
@@ -1,13 +1,15 @@
 
 clear
 output=$(sudo ansible-playbook provisioning.yml)
+echo "$output"
 IP=$(echo "$output" | grep "0].private_ip" | cut -d '"' -f 4) #gets IP from output
 IP2=$(echo "$output" | grep "1].private_ip" | cut -d '"' -f 4) #gets IP from output
 
 echo "IPs $IP $IP2"
 
+#Need to escape variable inside with 
 config=$(cat << EOF
-\<VirtualHost *:80>
+<VirtualHost *:80>
     <Proxy "balancer://mycluster">
         BalancerMember http://$IP:80 loadfactor=5 timeout=1
         BalancerMember http://$IP2:80 loadfactor=5 timeout=1
@@ -20,8 +22,8 @@ config=$(cat << EOF
 
         #ServerAdmin webmaster@localhost
         #DocumentRoot /var/www/html
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF
 )
@@ -29,3 +31,4 @@ EOF
 echo "$config" > LBconfigure
 
 sudo ansible-playbook LBconfigure.yml
+sudo ansible-playbook webConfigure.yml
