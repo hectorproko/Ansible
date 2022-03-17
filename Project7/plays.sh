84ef55d767a821db5cd1c4547b19c21f1c13ba98
Hector Rodriguez <hectiquin2003@hotmail.com>
2022-03-17 01:08:58 -0400

    some changes

--- Project7/plays.sh
+++ Project7/plays.sh
@@ -5,8 +5,8 @@ echo "$output"
 IP=$(echo $output | grep private_ip | cut -d '"' -f 4) #gets IP from output
 sudo ansible-playbook NFSconfigure.yml
 sudo ansible-playbook webConfigure.yml --extra-vars "privateIP=$IP" 
-sudo ansible-playbook dbServer.yml
-sudo ansible-playbook dbClient.yml
+# sudo ansible-playbook dbServer.yml
+# sudo ansible-playbook dbClient.yml
 
 #IP=$(echo $output | grep private_ip | cut -d '"' -f 4)
 #sudo ansible-playbook webConfigure.yml --extra-vars "privateIP=172.31.94.32" 
